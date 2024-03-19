9ba9c43636903fde9408606b46b22cfd21dc5ef2
Turbold Batsukh <turbold.flutter@gmail.com>
2024-03-19 21:47:36 +0800

    splashscreen jinhen bolgoloo

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -17,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen>
     super.initState();
     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
-    Future.delayed(Duration(seconds: 2), () {
+    Future.delayed(Duration(seconds: 5), () {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
         builder: (_) => Home(),
       ));
